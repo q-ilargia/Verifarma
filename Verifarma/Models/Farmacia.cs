b0ad5ec7cffea5aa49735d6f164edd307220ac43
VI <43181089+q-ilargia@users.noreply.github.com>
2024-07-24 20:15:18 +0100

    Test commit

--- Verifarma/Models/Farmacia.cs
+++ Verifarma/Models/Farmacia.cs
@@ -1,7 +1,22 @@
-﻿namespace Verifarma.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Verifarma.Models
 {
     public class Farmacia
     {
+        [Key]
+        public int Id { get; set; }
 
+        [Required]
+        public string Nombre { get; set; } = null!;
+
+        [Required]
+        public string Direccion { get; set; } = null!;
+
+        [Required]
+        public float Latitud { get; set; }
+
+        [Required]
+        public float Longitud { get; set; }
     }
 }
